b6ff986faeb227cc0733362c4f5c605586f55793
dotamind-ai <whoamivdk@yandex.ru>
2026-07-24 16:14:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Memora AI запущен");
+const addMemoryButton = document.getElementById("addMemory");
+
+
+addMemoryButton.addEventListener("click", function () {
+
+    const memory = prompt("Что ты хочешь сохранить в память?");
+
+
+    if (memory) {
+
+        alert(
+            "Память сохранена:\n\n" + memory
+        );
+
+    }
+
+});
