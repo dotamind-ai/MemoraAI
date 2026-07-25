697a504069d28abc4111e2f49c46f6a09e293a4c
dotamind-ai <whoamivdk@yandex.ru>
2026-07-25 13:54:11 +0300

    Create calendar.js

---
+++ calendar.js
@@ -0,0 +1,211 @@
+let currentMonth = new Date().getMonth();
+let currentYear = new Date().getFullYear();
+
+
+
+function renderCalendar(){
+
+
+const grid = document.getElementById("calendarGrid");
+const title = document.getElementById("monthTitle");
+
+
+
+if(!grid || !title){
+
+return;
+
+}
+
+
+
+const months = [
+
+"Январь",
+"Февраль",
+"Март",
+"Апрель",
+"Май",
+"Июнь",
+"Июль",
+"Август",
+"Сентябрь",
+"Октябрь",
+"Ноябрь",
+"Декабрь"
+
+];
+
+
+
+title.innerHTML = months[currentMonth] + " " + currentYear;
+
+
+
+grid.innerHTML = "";
+
+
+
+const weekDays = [
+
+"Пн",
+"Вт",
+"Ср",
+"Чт",
+"Пт",
+"Сб",
+"Вс"
+
+];
+
+
+
+weekDays.forEach(day=>{
+
+
+let item=document.createElement("div");
+
+item.className="day-name";
+
+item.innerHTML=day;
+
+
+grid.appendChild(item);
+
+
+});
+
+
+
+let firstDay = new Date(
+currentYear,
+currentMonth,
+1
+).getDay();
+
+
+
+if(firstDay===0){
+
+firstDay=7;
+
+}
+
+
+
+let totalDays = new Date(
+currentYear,
+currentMonth + 1,
+0
+).getDate();
+
+
+
+for(let i=1;i<firstDay;i++){
+
+
+let empty=document.createElement("div");
+
+grid.appendChild(empty);
+
+
+}
+
+
+
+
+
+for(let day=1; day<=totalDays; day++){
+
+
+let item=document.createElement("div");
+
+
+item.className="calendar-day";
+
+
+item.innerHTML=day;
+
+
+
+let today = new Date();
+
+
+
+if(
+
+day===today.getDate() &&
+
+currentMonth===today.getMonth() &&
+
+currentYear===today.getFullYear()
+
+){
+
+item.classList.add("today");
+
+
+}
+
+
+
+grid.appendChild(item);
+
+
+
+}
+
+
+
+}
+
+
+
+
+function changeMonth(value){
+
+
+currentMonth += value;
+
+
+
+if(currentMonth < 0){
+
+currentMonth = 11;
+
+currentYear--;
+
+}
+
+
+
+if(currentMonth > 11){
+
+currentMonth = 0;
+
+currentYear++;
+
+}
+
+
+
+renderCalendar();
+
+
+}
+
+
+
+
+
+document.addEventListener(
+
+"DOMContentLoaded",
+
+()=>{
+
+renderCalendar();
+
+}
+
+);
