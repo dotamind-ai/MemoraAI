3821510867c836cb230ca99a8039474f616e2491
dotamind-ai <whoamivdk@yandex.ru>
2026-07-25 16:04:11 +0300

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,5 +1,4 @@
 const months = [
-
 "Январь",
 "Февраль",
 "Март",
@@ -12,12 +11,10 @@ const months = [
 "Октябрь",
 "Ноябрь",
 "Декабрь"
-
 ];
 
 
 let today = new Date();
-
 
 let currentMonth = today.getMonth();
 
@@ -25,10 +22,7 @@ let currentYear = today.getFullYear();
 
 
 
-
-
 function createCalendar(){
-
 
 
 const grid = document.getElementById("calendarGrid");
@@ -36,19 +30,13 @@ const grid = document.getElementById("calendarGrid");
 const title = document.getElementById("monthTitle");
 
 
-
 grid.innerHTML = "";
-
 
 
 title.textContent =
 months[currentMonth] + " " + currentYear;
 
 
-
-
-
-// первый день месяца
 
 let firstDay = new Date(
 currentYear,
@@ -58,14 +46,13 @@ currentMonth,
 
 
 
-firstDay = firstDay === 0 ? 6 : firstDay - 1;
+if(firstDay === 0){
+
+firstDay = 7;
+
+}
 
 
-
-
-
-
-// количество дней
 
 let totalDays = new Date(
 currentYear,
@@ -75,182 +62,88 @@ currentMonth + 1,
 
 
 
+for(let i = 1; i < firstDay; i++){
 
+let empty = document.createElement("div");
 
-
-
-// пустые клетки перед первым числом
-
-for(let i = 0; i < firstDay; i++){
-
-
-let empty =
-document.createElement("div");
-
-
-empty.className =
-"calendar-empty";
-
+empty.className = "calendar-empty";
 
 grid.appendChild(empty);
-
 
 }
 
 
-
-
-
-
-
-
-// дни месяца
 
 for(let day = 1; day <= totalDays; day++){
 
 
-
-let cell =
-document.createElement("div");
+let cell = document.createElement("div");
 
 
-
-cell.className =
-"calendar-day";
+cell.className = "calendar-day";
 
 
-
-cell.textContent =
-day;
+cell.textContent = day;
 
 
-
-
-
-
-
-// сегодня
 
 if(
-
 day === today.getDate() &&
 currentMonth === today.getMonth() &&
 currentYear === today.getFullYear()
-
 ){
-
 
 cell.classList.add("today");
 
-
 }
-
-
-
-
 
 
 
 grid.appendChild(cell);
 
 
+}
+
 
 }
 
 
 
-}
 
-
-
-
-
-
-
-
-// назад месяц
-
-document
-.getElementById("prevMonth")
-.onclick = function(){
-
-
+document.getElementById("prevMonth").onclick = function(){
 
 currentMonth--;
 
-
-
-
-
 if(currentMonth < 0){
-
 
 currentMonth = 11;
 
 currentYear--;
 
-
 }
 
-
-
-
 createCalendar();
-
-
 
 };
 
 
 
-
-
-
-
-
-// вперёд месяц
-
-document
-.getElementById("nextMonth")
-.onclick = function(){
-
-
+document.getElementById("nextMonth").onclick = function(){
 
 currentMonth++;
 
-
-
-
-
 if(currentMonth > 11){
-
 
 currentMonth = 0;
 
 currentYear++;
 
-
 }
 
-
-
-
 createCalendar();
-
-
 
 };
 
 
 
-
-
-
-
-window.onload = function(){
-
-
-createCalendar();
-
-
-};
+window.onload = createCalendar;
