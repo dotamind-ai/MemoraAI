52ba0dd65f93fbe6e12c49c64302d461df018854
dotamind-ai <whoamivdk@yandex.ru>
2026-07-25 17:52:13 +0300

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,4 +1,5 @@
 const months = [
+
 "Январь",
 "Февраль",
 "Март",
@@ -11,14 +12,19 @@ const months = [
 "Октябрь",
 "Ноябрь",
 "Декабрь"
+
 ];
+
 
 
 let date = new Date();
 
 
 
+
+
 function drawCalendar(){
+
 
 
 const grid =
@@ -34,9 +40,13 @@ grid.innerHTML="";
 
 
 
-let month=date.getMonth();
+let month =
+date.getMonth();
 
-let year=date.getFullYear();
+
+let year =
+date.getFullYear();
+
 
 
 
@@ -45,27 +55,39 @@ months[month]+" "+year;
 
 
 
-let first =
+
+
+let firstDay =
 new Date(year,month,1).getDay();
 
 
 
-if(first===0)
-first=7;
+if(firstDay===0){
+
+firstDay=7;
+
+}
 
 
 
-for(let i=1;i<first;i++){
 
 
-let empty=document.createElement("div");
+for(let i=1;i<firstDay;i++){
 
-empty.className="calendar-empty";
+
+let empty =
+document.createElement("div");
+
+
+empty.className =
+"calendar-empty";
+
 
 grid.appendChild(empty);
 
 
 }
+
 
 
 
@@ -81,26 +103,40 @@ new Date(year,month+1,0).getDate();
 for(let i=1;i<=days;i++){
 
 
-let day=document.createElement("div");
+
+let day =
+document.createElement("div");
 
 
-day.className="calendar-day";
+
+day.className =
+"calendar-day";
+
 
 
 day.textContent=i;
 
 
 
-let now=new Date();
+
+
+let now =
+new Date();
 
 
 
 if(
+
 i===now.getDate()
+
 &&
+
 month===now.getMonth()
+
 &&
+
 year===now.getFullYear()
+
 ){
 
 day.classList.add("today");
@@ -109,102 +145,12 @@ day.classList.add("today");
 
 
 
+
 grid.appendChild(day);
 
 
-}
-
-
 
 }
-
-
-
-
-document.getElementById("prevMonth").onclick=()=>{
-
-date.setMonth(date.getMonth()-1);
-
-drawCalendar();
-
-};
-
-
-
-document.getElementById("nextMonth").onclick=()=>{
-
-date.setMonth(date.getMonth()+1);
-
-drawCalendar();
-
-};
-
-
-
-
-
-
-// НАВИГАЦИЯ
-
-
-const buttons =
-document.querySelectorAll(".nav-item");
-
-
-
-const pages =
-document.querySelectorAll(".page");
-
-
-
-
-function openPage(name){
-
-
-
-pages.forEach(page=>{
-
-page.classList.add("hidden");
-
-});
-
-
-
-if(name==="home")
-document.getElementById("homePage")
-.classList.remove("hidden");
-
-
-
-if(name==="calendar")
-document.querySelector(".calendar-container")
-.classList.remove("hidden");
-
-
-
-if(name==="events")
-document.getElementById("eventsPage")
-.classList.remove("hidden");
-
-
-
-if(name==="settings")
-document.getElementById("settingsPage")
-.classList.remove("hidden");
-
-
-
-
-buttons.forEach(btn=>{
-
-btn.classList.remove("active");
-
-});
-
-
-
-event.currentTarget?.classList.add("active");
-
 
 
 }
@@ -213,28 +159,41 @@ event.currentTarget?.classList.add("active");
 
 
 
-buttons.forEach(button=>{
+document
+.getElementById("prevMonth")
+.onclick=()=>{
 
 
-button.onclick=(e)=>{
-
-
-buttons.forEach(b=>b.classList.remove("active"));
-
-
-button.classList.add("active");
-
-
-openPage(
-button.dataset.page
+date.setMonth(
+date.getMonth()-1
 );
 
 
+drawCalendar();
+
 
 };
 
 
-});
+
+
+
+
+document
+.getElementById("nextMonth")
+.onclick=()=>{
+
+
+date.setMonth(
+date.getMonth()+1
+);
+
+
+drawCalendar();
+
+
+};
+
 
 
 
