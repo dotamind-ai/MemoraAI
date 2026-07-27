c3e70cecd085fd3874bd254f3b96959723384722
dotamind-ai <whoamivdk@yandex.ru>
2026-07-27 19:34:47 +0300

    Update auth.js

--- welcome/auth.js
+++ welcome/auth.js
@@ -464,3 +464,49 @@ window.addEventListener(
 
 
 });
+// MEMORA ENERGY BACKGROUND
+
+
+window.addEventListener("load",()=>{
+
+
+let ball =
+document.createElement("div");
+
+
+ball.className="energy-ball";
+
+
+document.body.appendChild(ball);
+
+
+
+for(let i=0;i<25;i++){
+
+
+let star =
+document.createElement("div");
+
+
+star.className="star";
+
+
+star.style.left =
+Math.random()*100+"vw";
+
+
+star.style.top =
+Math.random()*100+"vh";
+
+
+star.style.animationDelay =
+Math.random()*5+"s";
+
+
+document.body.appendChild(star);
+
+
+}
+
+
+});
